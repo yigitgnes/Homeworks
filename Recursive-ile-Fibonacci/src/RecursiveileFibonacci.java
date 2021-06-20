033d9674182c0265b4cba01639286f516e44ab3b
Yigit Mustafa Gunes <yigit.gnes@gmail.com>
2021-06-20 17:14:33 +0300

    <commit message>

---
+++ Recursive-ile-Fibonacci/src/RecursiveileFibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RecursiveileFibonacci {
+
+
+    public class Main {
+
+        static int fibo(int numb) {
+            if (numb == 1 || numb == 2) {
+                return 1;
+            }
+            return fibo(numb - 1) + fibo(numb - 2);
+        }
+
+        public static void main(String[] args) {
+            System.out.print("Bir sayı giriniz: ");
+            Scanner scan = new Scanner(System.in);
+            int numb = scan.nextInt();
+            System.out.println("Fiboniacci Sonucu: " + fibo(numb));
+        }
+    }
+}
